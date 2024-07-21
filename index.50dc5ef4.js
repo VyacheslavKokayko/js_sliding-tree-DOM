2fc6bf82f7cfbb95220d4a2b2bee8723220fce7b
Vyacheslav Kokayko <vkokayko@gmail.com>
2024-07-21 13:37:35 +0200

    make build

---
+++ index.50dc5ef4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".tree");e.addEventListener("click",function(e){var n=e.target.nextSibling;n&&(n.hidden=!n.hidden)}),e.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)});
+//# sourceMappingURL=index.50dc5ef4.js.map
